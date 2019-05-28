853fe886acbf005c85c5ead23d66f27aa8e47c46
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-05-28 13:07:15 +0100

    dropdown populated with country names

--- week_08_weekend/bucket-list/src/components/SelectCountry.js
+++ week_08_weekend/bucket-list/src/components/SelectCountry.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SelectCountry extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedCountry: []
+      selectedCountry: ''
     }
+
+    this.handleSelect = this.handleSelect.bind(this);
+
   }
+
+  componentDidMount(){
+    this.props.getData();
+  }
+
+  createOptions(){
+    return this.props.data.map((country, index) => {
+      return <option value={index}>{country.name}</option>
+    })
+  }
+
+  handleSelect(event){
+
+    this.props.updateBucketList(event.target.value);
+  }
+
+
+
+
 
 
   render(){
     return(
       <div>
       <label>Add to bucket-list</label>
-      <select></select>
+      <select onChange={this.handleSelect}>{this.createOptions()}</select>
       </div>
     )
   }
@@ -23,7 +46,44 @@ class SelectCountry extends Component {
 
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return{
+    getData: () => {
+      dispatch(() => {
+        fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data =>{
+          const updatedData = data.map((country) => {
+            country.bucketList = false
+            return country
+          })
+          dispatch({
+            type: 'ADD_DATA',
+            data: updatedData
+          })
+        })
+      })
+    },
+    updateBucketList: (index) => {
+      console.log('working');
+      dispatch({
+        type: 'ADD_TO_BUCKETLIST',
+        index: index
+      })
+    }
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return{
+
+    data: state
+
+  }
+}
 
 
 
-export default SelectCountry;
+
+export default connect(mapStateToProps, mapDispatchToProps)(SelectCountry);
--- week_08_weekend/bucket-list/src/reducers/countriesReducer.js
+++ week_08_weekend/bucket-list/src/reducers/countriesReducer.js
@@ -1,5 +1,15 @@
 const countriesReducer = (state = [], action) => {
+switch(action.type){
+  case 'ADD_DATA':
+  console.log(action.data);
+  return [...state, ...action.data]
+  case 'ADD_TO_BUCKETLIST':
+  
+  default:
   return state
+}
+
+
 }
 
 
